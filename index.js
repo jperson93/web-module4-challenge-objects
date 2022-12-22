2bc16ff589ae7d5d6026e5e03e879b55946e9023
Jb Person <homeaccount@Jbs-MacBook-Air.local>
2022-12-21 21:01:11 -0500

    task 2 done

--- index.js
+++ index.js
@@ -53,8 +53,22 @@ const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
+
+  discount: function (person){
+    if (person === 'teacher' || person ==='student'){
+      return this.price - (this.price * 0.25) 
+    }
+    else if (person === 'public'){
+      return this.price - (this.price *0.10)
+    }
+  }
   
 }
+
+console.log(burger.discount('public'));
+console.log(burger.discount('teacher'));
+
+
 
 
 
